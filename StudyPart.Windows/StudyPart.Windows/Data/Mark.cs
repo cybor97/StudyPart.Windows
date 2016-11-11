b4e654ae26309528b2d5891cc005c7d5e24e36dd
Cybor <muhametshin.1997@gmail.com>
2016-11-12 00:41:22 +0300

    Made equality methods overrides. Implemented base conversion operations.

--- StudyPart.Windows/StudyPart.Windows/Data/Mark.cs
+++ StudyPart.Windows/StudyPart.Windows/Data/Mark.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 
 namespace StudyPart.Windows.Data
 {
@@ -13,5 +14,75 @@ namespace StudyPart.Windows.Data
         public int MarkValue { get; set; }
 
         public int YearHalf { get; set; }
+
+        public static bool operator ==(Mark v1, Mark v2)
+        {
+            return v1.Equals(v2);
+        }
+
+        public static bool operator !=(Mark v1, Mark v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Mark mark = (Mark)obj;
+            return Subject == mark.Subject &&
+                TestDate == mark.TestDate &&
+                MarkValue == mark.MarkValue &&
+                YearHalf == mark.YearHalf;
+        }
+
+        //IDK what to do with it. It's not recommended to use dinamic properties here... So let it be default.
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static Mark Parse(string xml)
+        {
+            try
+            {
+                var result = new Mark();
+                Utils.ReadXMLString(xml, reader =>
+                 {
+                     while (!reader.EOF)
+                         if (reader.IsStartElement(nameof(Mark)))
+                         {
+                             result.ID = long.Parse(reader.GetAttribute(nameof(ID)));
+                             result.TestDate = DateTime.Parse(reader.GetAttribute(nameof(TestDate)));
+                             result.MarkValue = int.Parse(reader.GetAttribute(nameof(MarkValue)));
+                             result.YearHalf = int.Parse(reader.GetAttribute(nameof(YearHalf)));
+                         }
+                         else if (reader.IsStartElement(nameof(Subject)))
+                             result.Subject = Subject.Parse(reader.ReadOuterXml());
+                 });
+                return result;
+            }
+            catch (XmlException)
+            {
+                return null;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Utils.WriteXMLString((ref XmlWriter writer) =>
+            {
+                writer.WriteStartElement(nameof(Mark));
+                writer.WriteAttributeString(nameof(ID), ID.ToString());
+                writer.WriteAttributeString(nameof(TestDate), TestDate.ToString());
+                writer.WriteAttributeString(nameof(MarkValue), MarkValue.ToString());
+                writer.WriteAttributeString(nameof(YearHalf), YearHalf.ToString());
+
+                writer.WriteElementString(nameof(Subject), Subject.ToString());
+
+                writer.WriteEndElement();
+            });
+        }
     }
 }
