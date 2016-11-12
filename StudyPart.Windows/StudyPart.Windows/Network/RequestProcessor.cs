5c963ba49141e02860ca2ede3286d75da18c6dca
Cybor <muhametshin.1997@gmail.com>
2016-11-13 01:40:36 +0300

    Picked base prototype of request processor.

--- StudyPart.Windows/StudyPart.Windows/Network/RequestProcessor.cs
+++ StudyPart.Windows/StudyPart.Windows/Network/RequestProcessor.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudyPart.Windows.Network
+﻿namespace StudyPart.Windows.Network
 {
-    class RequestProcessor
+    public static class RequestProcessor
     {
+        public static Response Process(this Request req)
+        {
+            var result = new Response();
+            switch (req.Type)
+            {
+                case "TEST": return new Response("OK");
+                case "GET":
+                    switch (req.In)
+                    {
+                    }
+                    break;
+                case "ADD":
+                    switch (req.In)
+                    {
+                    }
+                    return new Response("OK");
+                case "SET":
+                    switch (req.In)
+                    {
+                    }
+                    return new Response("OK");
+                case "DELETE":
+                case "REMOVE":
+                    switch (req.In)
+                    {
+                    }
+                    return new Response("OK");
+            }
+            return result;
+        }
     }
 }
