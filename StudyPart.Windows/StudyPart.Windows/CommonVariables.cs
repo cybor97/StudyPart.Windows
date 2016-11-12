0f3c7958998dd3005b6ce36114f2fd0d394e29cc
Cybor <muhametshin.1997@gmail.com>
2016-11-13 01:39:57 +0300

    Created port number common variable.

---
+++ StudyPart.Windows/StudyPart.Windows/CommonVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyPart.Windows
+{
+    class CommonVariables
+    {
+        public static int ServerPortNumber = 12006;
+    }
+}
