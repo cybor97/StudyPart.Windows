e613615fede6f71a78b1713cffc682fa3c65b8a8
Cybor <muhametshin.1997@gmail.com>
2016-11-15 10:52:30 +0300

    Updated parsing method.

--- StudyPart.Windows/StudyPart.Windows/Network/Request.cs
+++ StudyPart.Windows/StudyPart.Windows/Network/Request.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Text;
-using System.Xml;
+﻿using System.Xml;
 
 namespace StudyPart.Windows.Network
 {
@@ -19,24 +17,38 @@ namespace StudyPart.Windows.Network
             Request result = new Request();
             try
             {
-                using (var reader = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(xml))))
-                    while (!reader.EOF)
-                        if (reader.IsStartElement(nameof(Type)))
-                            result.Type = reader.ReadElementContentAsString();
-                        else if (reader.IsStartElement(nameof(In)))
-                            result.In = reader.ReadElementContentAsString();
-                        else if (reader.IsStartElement(nameof(By)))
-                            result.By = reader.ReadElementContentAsString();
-                        else if (reader.IsStartElement(nameof(Data)))
-                            result.Data = reader.ReadInnerXml();
-                        else
-                            reader.Read();
+                Utils.ReadXMLString(xml, reader =>
+                 {
+                     while (!reader.EOF)
+                         if (reader.IsStartElement(nameof(Type)))
+                             result.Type = reader.ReadElementContentAsString();
+                         else if (reader.IsStartElement(nameof(In)))
+                             result.In = reader.ReadElementContentAsString();
+                         else if (reader.IsStartElement(nameof(By)))
+                             result.By = reader.ReadElementContentAsString();
+                         else if (reader.IsStartElement(nameof(Data)))
+                             result.Data = reader.ReadInnerXml();
+                         else
+                             reader.Read();
+                 });
             }
             catch (XmlException)
             {
                 //Console.WriteLine(e);
             }
             return result;
+        }
+        public override string ToString()
+        {
+            return Utils.WriteXMLString((ref XmlWriter writer) =>
+            {
+                writer.WriteStartElement(nameof(Request));
+                writer.WriteElementString(nameof(Type), Type);
+                writer.WriteElementString(nameof(In), In);
+                writer.WriteElementString(nameof(By), By);
+                writer.WriteElementString(nameof(Data), Data);
+                writer.WriteEndElement();
+            });
         }
     }
 }
